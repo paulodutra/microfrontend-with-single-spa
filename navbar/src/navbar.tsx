8a259ce3207afab64c9e38fa605ab195c6f95ce3
paulodutra <paulodutra.info@gmail.com>
2024-01-23 23:11:12 -0300

    chore: configuring project

---
+++ navbar/src/navbar.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import * as singleSpa from 'single-spa';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: any) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function SimpleTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+    switch (newValue) {
+        case 0:
+            singleSpa.navigateToUrl('/');
+            break;
+        case 1:
+            singleSpa.navigateToUrl('/app1');
+            break;
+        case 2:
+            singleSpa.navigateToUrl('/app2')
+            break;
+        default:
+            break;
+    }
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Home" {...a11yProps(0)} />
+          <Tab label="App 1" {...a11yProps(1)} />
+          <Tab label="App 2" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Home
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        App 1
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        App 2
+      </TabPanel>
+    </div>
+  );
+}
---
+++ navbar/src/root.component.tsx
@@ -0,0 +1,5 @@
+import SimpleTabs from './navbar';
+
+export default function Root(props) {
+  return SimpleTabs();
+}
